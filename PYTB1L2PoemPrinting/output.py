04aa7e058ef5428cb4acaffa2f69453d449cb35b
Bart <Bartdb2001@gmail.com>
2020-09-09 14:23:29 +0200

    Poem excersise finished and uploaded

---
+++ PYTB1L2PoemPrinting/output.py
@@ -0,0 +1,12 @@
+>>> vers1 = "Mijn naam is Bart de Boer."
+>>> vers2 = "Van Python snap ik nog geen ene moer."
+>>> vers3 = "Ik ben 18 jaar."
+>>> vers4 = "En ik ben eindelijk met mijn middelbare school klaar."
+>>> vers5 = "Ik besteed veel tijd met gamen."
+>>> vers6 = "En ik zou graag mijn favorieten willen delen."
+>>> vers7 = "Mijn favoriet nu is Phantasy Star Online 2."
+>>> vers8 = "En Team fortress 2 kan ik ook naar toe."
+>>> vers9 = "Maar nu schrijf ik hier een gedicht."
+>>> vers10 = "Al ben ik hier niet echt voor geschikt."
+>>> print(vers1,vers2,vers3,vers4,vers5,vers6,vers7,vers8,vers9,vers10)
+Mijn naam is Bart de Boer. Van Python snap ik nog geen ene moer. Ik ben 18 jaar. En ik ben eindelijk met mijn middelbare school klaar. Ik besteed veel tijd met gamen. En ik zou graag mijn favorieten willen delen. Mijn favoriet nu is Phantasy Star Online 2. En Team fortress 2 kan ik ook naar toe. Maar nu schrijf ik hier een gedicht. Al ben ik hier niet echt voor geschikt.
