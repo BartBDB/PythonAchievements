6cce413908a4de49818c09b4b09b4a1a06d3889f
Bart <Bartdb2001@gmail.com>
2020-09-16 15:06:10 +0200

    Slice and Dice

---
+++ PYTB1L2SliceWise/Slicer.py
@@ -0,0 +1,21 @@
+tekst = 'De Python lessen worden gegeven voor Erik, Erwin en ook door Hidde'
+SlicedText1 = tekst[37:41]
+SlicedText2 = tekst[43:48]
+SlicedText3 = tekst[61:66]
+print(SlicedText1 + ", " + SlicedText2 + " en " + SlicedText3)
+
+tekst = "SD vakken zijn Python, UXD, Frontend development en Backend development en nog veel meer ..."
+SlicedText1 = tekst[15:21]
+SlicedText2 = tekst[23:26]
+SlicedText3 = tekst[28:48]
+SlicedText4 = tekst[52:71]
+print(SlicedText1 + ", " + SlicedText2 + ", " + SlicedText3 + " en " + SlicedText4)
+
+tekst = "Wat is hier het laatste woord?"
+SlicedText1 = tekst[24:30]
+print(SlicedText1)
+
+tekst = "Het www is ontwikkeld vanaf 1989 door Tim Berners-Lee"
+SlicedText1 = tekst[4:7]
+SlicedText2 = tekst[28:31]
+print(SlicedText1 + ", " + SlicedText2)
