fee9972ea199fe7564e8bc930a0ad5f496c48da1
Bart <Bartdb2001@gmail.com>
2020-09-16 15:25:21 +0200

    Werken met variabelen

---
+++ PYTB1L2InVariablesWeTrust/Variabelen.py
@@ -0,0 +1,20 @@
+#Character
+character = "/"
+#Float
+floater = 5,5
+#booleans
+boolean1 = True
+boolean2 = boolean1
+#integers
+integer1 = 20
+integer2 = 3
+integer3 = integer1 - integer2
+#strings
+string1 = "Hallo, ik ben"
+string2 = " Bart de Boer"
+string3 = string1 + string2
+print(character)
+print(floater)
+print(boolean2)
+print(integer3)
+print(string3)
