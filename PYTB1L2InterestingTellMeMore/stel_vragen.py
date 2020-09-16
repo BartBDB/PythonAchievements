427698c34be17cced97b2ed469739b4d724cb0bf
Bart <Bartdb2001@gmail.com>
2020-09-16 14:39:34 +0200

    Script voor vragen

---
+++ PYTB1L2InterestingTellMeMore/stel_vragen.py
@@ -0,0 +1,7 @@
+print("Hallo, gebruiker!")
+naam = input("Wat is jouw naam? ")
+print("Aha, hallo " + naam + "! Nu word ik nieuwsgierig.")
+game = input("Wat is jouw favoriete game? ")
+print("Dus jouw favourite game is " + game + "? Mooi.")
+tijd = input("Hoe vaak heb jij het de laatste week gespeeld? ")
+print("Dus jij hebt " + game + "deze week al " + tijd + " gespeeld.")
