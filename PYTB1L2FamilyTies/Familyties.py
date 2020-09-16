b73034e006b4c54991c0ab2b022e338ea9de5d46
Bart <Bartdb2001@gmail.com>
2020-09-16 14:51:23 +0200

    Script over mijzelf

---
+++ PYTB1L2FamilyTies/Familyties.py
@@ -0,0 +1,7 @@
+Age = 18
+Name = "Bart de Boer"
+Distance = 30
+Place = "Mijdrecht"
+
+print("Hallo, ik ben " + Name + ". Ik ben " + str(Age) + " jaar.")
+print("Ik woon in " + Place + " en dat is " + str(Distance) + " kilometer hiervandaan.")
