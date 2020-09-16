d217188fc8bc3c65d2f219a8bca02cd232c574eb
Bart <Bartdb2001@gmail.com>
2020-09-16 14:01:02 +0200

    Player Statistics

---
+++ PYTB1L2PlayerStatistics/Statistics.py
@@ -0,0 +1,20 @@
+name = "Bart de Boer" #string
+Speed = 50 #integer
+Jump = 10.5 #float/double
+Wanted = False #boolean
+Car = "none"
+Lives = 3
+Ammo = 2
+Level = 571
+School = "Mediacollege"
+Character = "/"
+print("Name: " + name)
+print("Speed: " + str(Speed))
+print("Jump: " + str(Jump))
+print("Wanted: " + str(Wanted))
+print("Car: " + Car)
+print("Lives: " + str(Lives))
+print("Ammo: " + str(Ammo))
+print("Level: " + str(Level))
+print("School: " + School)
+print("Favourite character: " + Character)
