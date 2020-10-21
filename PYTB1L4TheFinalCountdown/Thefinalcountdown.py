27ecc6cd59a5dea8b509a647248f8a6f4c974404
Bart <Bartdb2001@gmail.com>
2020-10-21 14:00:42 +0200

    I prefer the song, but this is cool too.

--- PYTB1L4TheFinalCountdown/Thefinalcountdown.py
+++ PYTB1L4TheFinalCountdown/Thefinalcountdown.py
@@ -1,10 +1,14 @@
+import time
+
 isRunning = True
 getal = input("Vanaf welk getal tellen wij af? ")
 #Je kan natuurlijk ook een getal hier invoeren in plaats van input(), maar dit is leuker.
 
 while (isRunning == True):
     getal = int(getal) - 1
+    time.sleep(0.01)
     #Als wij input() niet gebruiken, moet int() weg hierboven.
+    print(getal)
     if getal == 0:
-        print("Never gonna give you up. Never gonna let you down. Never gonna run around and desert you. Never gonna make you cry. Never gonna say goodbye. Never gonna tell a lie and hurt you")
+        print("En nu zitten wij op 0.")
         exit() #Dit zorgt ervoor dat het programma ook afsluit en je weer verder kan met je commandline.
